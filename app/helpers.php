c6980ef5407caf4b61e4a2e30a7bd407091a6e11
Bernard Historillo <bernardhistorillo1@gmail.com>
2023-08-09 15:17:37 +0800

    Update OG image and icon

--- app/helpers.php
+++ app/helpers.php
@@ -7,7 +7,7 @@ if(!function_exists('ogDetails')) {
     function ogDetails() {
         if(Route::currentRouteName() == 'home.index') {
             $data['description'] = 'Sign Up & Get 20% Discount On Our Launch!';
-            $data['image'] = asset('img/home/og-3.jpg');
+            $data['image'] = asset('img/home/og-4.jpg');
         } else {
             $data['description'] = 'We are BARE.';
             $data['image'] = asset('img/home/og-2.jpg');
