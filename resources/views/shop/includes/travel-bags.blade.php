166296ed3d9ad742aa5a55e2bfd64305e7b5bae4
Bernard Historillo <bernardhistorillo1@gmail.com>
2023-12-11 17:44:37 +0800

    Remove Zip Bag from travel bags section

--- resources/views/shop/includes/travel-bags.blade.php
+++ resources/views/shop/includes/travel-bags.blade.php
@@ -2,33 +2,33 @@
     <div class="container-fluid py-5">
         <div id="carouselExample" class="carousel slide lg:tw-px-[120px] xl:tw-px-[150px]">
             <div class="carousel-inner">
+{{--                <div class="carousel-item active">--}}
+{{--                    <div class="row justify-content-center align-items-center">--}}
+{{--                        <div class="col-11 col-sm-8 col-md-7 col-lg-6">--}}
+{{--                            <div class="card mb-5 mb-lg-0" style="background-color:rgba(0,0,0,0); border:0">--}}
+{{--                                <div class="card-body p-0">--}}
+{{--                                    <img src="{{ asset('img/shop/products/zip-bag.webp') }}" class="w-100" />--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+
+{{--                        <div class="col-lg-6">--}}
+{{--                            <div class="ps-md-4">--}}
+{{--                                <div class="ps-xl-4 ps-xxl-5">--}}
+{{--                                    <p class="text-white cerebri-sans-pro-medium text-center text-lg-start font-size-180 font-size-sm-200 font-size-md-220 line-height-130 mb-3 mb-lg-2">ZIP BAG</p>--}}
+
+{{--                                    <p class="text-white cerebri-sans-pro-regular text-center text-lg-start font-size-110 font-size-md-120 font-size-lg-130 font-size-xl-140 font-size-xxl-150 mb-5">Stay organized and prepared with our complimentary Ziplock Bags. These handy storage solutions are made from tough materials, ensuring both practicality and visibility. Their small size fits perfectly in your purse, gym bag, or suitcase, guaranteeing that your nipple covers are always within reach. A considerate addition to your purchase, offering convenience and peace of mind.</p>--}}
+
+{{--                                    <div class="text-center text-lg-start">--}}
+{{--                                        <a href="{{ route('shop.category', 'nipple-covers') }}" class="btn btn-custom-3 cerebri-sans-pro-bold letter-spacing-5 font-size-120 font-size-lg-130 font-size-xl-140 tw-pt-[8px] lg:tw-pt-[10px] px-4">SHOP NIPPLE COVERS</a>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
                 <div class="carousel-item active">
-                    <div class="row justify-content-center align-items-center">
-                        <div class="col-11 col-sm-8 col-md-7 col-lg-6">
-                            <div class="card mb-5 mb-lg-0" style="background-color:rgba(0,0,0,0); border:0">
-                                <div class="card-body p-0">
-                                    <img src="{{ asset('img/shop/products/zip-bag.webp') }}" class="w-100" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-6">
-                            <div class="ps-md-4">
-                                <div class="ps-xl-4 ps-xxl-5">
-                                    <p class="text-white cerebri-sans-pro-medium text-center text-lg-start font-size-180 font-size-sm-200 font-size-md-220 line-height-130 mb-3 mb-lg-2">ZIP BAG</p>
-
-                                    <p class="text-white cerebri-sans-pro-regular text-center text-lg-start font-size-110 font-size-md-120 font-size-lg-130 font-size-xl-140 font-size-xxl-150 mb-5">Stay organized and prepared with our complimentary Ziplock Bags. These handy storage solutions are made from tough materials, ensuring both practicality and visibility. Their small size fits perfectly in your purse, gym bag, or suitcase, guaranteeing that your nipple covers are always within reach. A considerate addition to your purchase, offering convenience and peace of mind.</p>
-
-                                    <div class="text-center text-lg-start">
-                                        <a href="{{ route('shop.category', 'nipple-covers') }}" class="btn btn-custom-3 cerebri-sans-pro-bold letter-spacing-5 font-size-120 font-size-lg-130 font-size-xl-140 tw-pt-[8px] lg:tw-pt-[10px] px-4">SHOP NIPPLE COVERS</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="carousel-item">
                     <div class="row justify-content-center align-items-center">
                         <div class="col-11 col-sm-8 col-md-7 col-lg-6">
                             <div class="card mb-5 mb-lg-0" style="background-color:rgba(0,0,0,0); border:0">
