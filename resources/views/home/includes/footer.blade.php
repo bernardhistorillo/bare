04053c4cf0bd1991711f776279dce4020a876591
Bernard Historillo <bernardhistorillo1@gmail.com>
2024-01-31 14:46:34 +0800

    Add lazada and shopee store links to footer

--- resources/views/home/includes/footer.blade.php
+++ resources/views/home/includes/footer.blade.php
@@ -1,14 +1,14 @@
 <div class="bg-color-7 py-5">
-    <div class="container py-4">
+    <div class="container py-3">
         <div class="row justify-content-center align-items-center">
             <div class="col-7 col-sm-5 col-md-3 mb-4 mb-md-0">
-                <div class="mb-4 mb-lg-5">
+                <div class="mb-4">
                     <a href="{{ route('home.index') }}" class="text-decoration-none">
                         <img src="{{ asset('img/home/bare-white.png') }}" class="w-100" alt="{{ config('app.name') }}" />
                     </a>
                 </div>
 
-                <div class="d-flex justify-content-center justify-content-md-start align-items-center">
+                <div class="d-flex justify-content-center justify-content-md-start align-items-center mb-4">
                     <div class="pe-4">
                         <a href="https://www.facebook.com/wearebare.co" target="_blank" class="text-white text-decoration-none">
                             <i class="fa-brands fa-facebook-f font-size-160 font-size-md-180 font-size-lg-220 font-size-xl-250"></i>
@@ -21,9 +21,23 @@
                         </a>
                     </div>
 
-                    <div class="">
+                    <div class="pe-4">
                         <a href="https://www.tiktok.com/@wearebare.co" target="_blank" class="text-white text-decoration-none">
                             <i class="fa-brands fa-tiktok font-size-160 font-size-md-180 font-size-lg-220 font-size-xl-250"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-center justify-content-md-start align-items-center">
+                    <div class="pe-4">
+                        <a href="https://www.lazada.com.ph/shop/wearebare-co" target="_blank" rel="noreferrer">
+                            <img src="{{ asset('img/home/lazada.webp') }}" class="tw-w-[50px] tw-rounded-[30%]" alt="{{ config('app.name') }}" />
+                        </a>
+                    </div>
+
+                    <div class="">
+                        <a href="https://shopee.ph/wearebare.co" target="_blank" rel="noreferrer">
+                            <img src="{{ asset('img/home/shopee.webp') }}" class="tw-w-[50px] tw-rounded-[30%]" alt="{{ config('app.name') }}" />
                         </a>
                     </div>
                 </div>
